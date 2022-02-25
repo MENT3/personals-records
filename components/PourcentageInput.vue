87314bd3506ad79d32129b374b0c5c1f036b86eb
Clément <clement.ramoslage@gmail.com>
2022-02-25 12:59:39 +0100

    feat: add pattern

--- components/PourcentageInput.vue
+++ components/PourcentageInput.vue
@@ -5,6 +5,7 @@
       class="block w-full rounded-md border border-slate-300 bg-white px-3 py-2 placeholder-slate-400 shadow-sm focus:border-sky-500 focus:outline-none focus:ring-1 focus:ring-sky-500 sm:text-sm"
       type="number"
       placeholder="Pourcentage"
+      pattern="[0-9]*"
     />
 
     <div class="mt-8 grid w-full grid-cols-5 gap-4">
