41e67c54a925fe529dc3c1f9d61464b0ef6ab9ad
Clément <clement.ramoslage@gmail.com>
2022-02-25 09:52:47 +0100

    feat: init store

---
+++ store/index.js
@@ -0,0 +1,46 @@
+export const state = () => ({
+  records: [
+    {
+      slug: 'deadlift',
+      name: 'Deadlift',
+      type: 'w',
+      record: 190
+    },
+    {
+      slug: 'clean',
+      name: 'Clean',
+      type: 'w',
+      record: 120
+    },
+    {
+      slug: 'snatch',
+      name: 'Snatch',
+      type: 'w',
+      record: 105
+    },
+    {
+      slug: 'squat',
+      name: 'Squat',
+      type: 'w',
+      record: 140
+    },
+    {
+      slug: 'jerk',
+      name: 'Jerk',
+      type: 'w',
+      record: 120
+    },
+    {
+      slug: 'c2b',
+      name: 'Chest To Bar',
+      type: 'r',
+      record: 120
+    }
+  ]
+})
+
+export const getters = {
+  getMouvementFromSlug: (state) => (slug) => {
+    return state.records.find((movement) => movement.slug === slug)
+  }
+}
